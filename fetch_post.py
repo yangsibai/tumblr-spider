b67922c6b9ccd6fe4bdef43642c88660a595b619
Yangsibai <yangsibai@gmail.com>
2015-08-23 23:52:16 +0800

    fetch src

--- fetch_post.py
+++ fetch_post.py
@@ -8,7 +8,12 @@ def run():
     with open('posts.txt', 'r') as f:
         lines = f.readlines()
         for l in lines:
-            fetch(l.strip())
+            url = l.strip()
+            src = fetch(url)
+            if src is not None:
+                print "source %s find at %s" % (src, url)
+            else:
+                print "no source at %s" % url
             return
     print "all done!"
 
@@ -16,6 +21,8 @@ def run():
 def fetch(url):
     print "fetching %s" % url
     page = get_real_page_data(url)
+    src = get_source_url(page)
+    return src
 
 
 def get_real_page_data(url):
@@ -29,6 +36,15 @@ def get_real_page_data(url):
 
 def get_iframe_url(data):
     srcReg = re.compile('<iframe src=[\'"]([^\'"]+)[\'"]', re.IGNORECASE | re.MULTILINE)
+    m = srcReg.search(data)
+    if m is not None:
+        return m.group(1)
+    return None
+
+
+def get_source_url(data):
+    srcReg = re.compile('<source src=[\'"]([^\'"]+)[\'"]', re.IGNORECASE |
+                        re.MULTILINE)
     m = srcReg.search(data)
     if m is not None:
         return m.group(1)
